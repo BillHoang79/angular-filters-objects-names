14f6202568d362d843d76d72c187427279381625
Bill Hoang <billhoang79@gmail.com>
2017-02-03 22:12:02 -0800

    filter and objects

---
+++ controllers/myctrl.js
@@ -0,0 +1,25 @@
+app.controller('myCtrl', function($scope) {
+	$scope.mydata = { 
+		arr: [
+		{
+			name: "jane",
+			age:34
+		},
+		{
+			name:"jake",
+			age:56
+		},
+		{
+			name:"jack",
+			age:23
+		},
+		{
+			name:"john",
+			age:45
+		},
+		{
+			name:"john",
+			age:32
+		}
+	]}
+})
